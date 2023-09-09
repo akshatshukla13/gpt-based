71afe00841f49169ede5ffe567224c9a0d17bc16
Akshat-Sh <103410083+akshatshukla13@users.noreply.github.com>
2023-09-09 17:04:44 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# Tutorial
+This is part gota gpt: 
+
+
+thanks to,
+https://youtu.be/AuxSJiv4NZU
+
+# ChatGPT4-API-Simple-Site
+ChatGPT4-API-Simple-Site
---
+++ script.js
@@ -0,0 +1,44 @@
+const form = document.getElementById('chat-form');
+const mytextInput = document.getElementById('mytext');
+const responseTextarea = document.getElementById('response');
+
+
+
+const API_KEY = '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';
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const mytext = 'According to geeta, hey gpt, give answer about' + mytextInput.value.trim();
+
+    if (mytext) {
+        try {
+            const response = await fetch('https://api.openai.com/v1/chat/completions', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${API_KEY}`,
+                },
+                body: JSON.stringify({
+                    model: 'gpt-3.5-turbo',
+                    messages: [{ role: 'user', content: mytext }],
+                    temperature: 1.0,
+                    top_p: 0.7,
+                    n: 1,
+                    stream: false,
+                    presence_penalty: 0,
+                    frequency_penalty: 0,
+                }),
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                responseTextarea.value = data.choices[0].message.content;
+            } else {
+                responseTextarea.value = 'Error: Unable to process your request.';
+            }
+        } catch (error) {
+            console.error(error);
+            responseTextarea.value = 'Error: Unable to process your request.';
+        }
+    }
+});
